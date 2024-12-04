862a72fbcc956b328263e5a22dce529ef3fccf83
Kumar Praneet <156080092+kpisop@users.noreply.github.com>
2024-12-04 17:57:49 +0530

    Add files via upload

---
+++ sketch_dec4a_copy_20241204175621.ino
@@ -0,0 +1,22 @@
+int moistureSensor = 9
+int led = 10
+int brightness = 0
+int pump = 11
+
+void setup() {
+  // Main Setup :-
+  pinMode(moistureSensor, INPUT)
+  pinMode(led, OUTPUT)
+  pinMode(pump, OUTPUT)
+  
+  // 
+  Serial.begin(9600)
+  
+}
+
+void loop() {
+  // Main code:-
+
+
+
+}
